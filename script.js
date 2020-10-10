5a760128f6ba2addbeec716c4a347c1aca78582b
Samuel-Lucas <samuel.moreira10@outlook.com>
2020-10-10 16:56:26 -0300

    update 2

--- script.js
+++ script.js
@@ -4,6 +4,24 @@ let notes = document.getElementById('tabela_notebooks')
 let videoGame = document.getElementById('tabela_vgs')
 let acessorios = document.getElementById('tabela_acs')
 
+function mudaEstilo2() {
+    let li = event.target
+
+    li.addEventListener("mouseenter", function() {
+        li.style.backgroundColor = 'gray'
+        li.style.color = 'white'
+        li.style.textDecoration = 'underline'
+        li.style.padding = '8px'
+    })
+
+    li.addEventListener("mouseout", function() {
+        li.style.backgroundColor = 'white'
+        li.style.color = 'black'
+        li.style.textDecoration = 'none'
+        li.style.padding = '0px'
+    })
+}
+
 pcs.style.display = 'none'
 notes.style.display = 'none'
 videoGame.style.display = 'none'
