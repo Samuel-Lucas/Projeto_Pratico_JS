f5b518b6b8d1ce4398f5087fe33ab262e24da260
Samuel-Lucas <samuel.moreira10@outlook.com>
2020-10-15 19:55:56 -0300

    ajustando função de exibição e menu de categorias

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-let todos = document.querySelector('#todos')
-let pcs = document.querySelector('#computadores')
-let notes = document.querySelector('#notebooks')
-let videoGame = document.querySelector('#videogames')
-let acessorios = document.querySelector('#acessorios')
-
-pcs.style.display = 'none'
-notes.style.display = 'none'
-videoGame.style.display = 'none'
-acessorios.style.display = 'none'
-
-function exibirCategoria(categoria) {
-    
-    let tabelas = document.querySelectorAll('table')
-    
-    for(let i = 0; i < tabelas.length; i++) {
-
-        if(tabelas[i].id == categoria) {
-
-            tabelas[i].style.display = 'block'
-
-        } else {
-            
-            tabelas[i].style.display = 'none'
-        }
-    }
-}
-
 function mudaEstilo() {
     let itemMenu = event.target
 
@@ -45,18 +17,37 @@ function mudaEstilo2() {
         li.style.backgroundColor = '#7575a3'
         li.style.color = 'white'
         li.style.textDecoration = 'underline'
-        li.style.padding = '8px'
+        li.style.lineHeight = '30px'
+        li.style.borderRadius = '10px'
     })
 
     li.addEventListener("mouseout", function() {
         li.style.backgroundColor = 'white'
         li.style.color = 'black'
         li.style.textDecoration = 'none'
-        li.style.padding = '0px'
+        li.style.lineHeight = '15px'
+        li.style.borderRadius = '0px'
     })
 }
 
-function alteraImg() {
+function exibirCategoria(categoria) {
+    
+    let tabelas = document.querySelectorAll('table')
+    
+    for(let i = 0; i < tabelas.length; i++) {
+
+        if(tabelas[i].id == categoria) {
+
+            tabelas[i].style.display = 'block'
+
+        } else {
+            
+            tabelas[i].style.display = 'none'
+        }
+    }
+}
+
+function abreImg() {
     let img = event.target
 
     let url = img.getAttribute('src')
